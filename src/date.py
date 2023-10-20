222b294515fafef0e77354ccf8ca43d9bbaff44c
mini109 <derdriumcateer@gmail.com>
2023-10-20 16:26:22 +1000

    added date function to add date into txt file

--- src/date.py
+++ src/date.py
@@ -1,1 +1,17 @@
 # will store all the functions relating to date and time 
+from datetime import datetime
+
+# Will read the date from the txt file (date the app was last accessed)
+def accessed_date():
+    global past_date
+    with open('date_last_accessed.txt') as f:
+        past_date = f.read()
+    return past_date
+
+# Will save current date into txt file once the app is quit
+def date_today():
+    with open('date_last_accessed.txt', 'w') as f:
+        current_date = str(datetime.now().date())
+        f.write(f'{current_date}')
+
+accessed_date()
--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
+from datetime import datetime
+import date
+
 # The user interface 
+def check_date():
+    if date.past_date == str(datetime.now().date()):
+        pass
+    else:
+        date.date_today()
+        print('TODAYS DATE ADDED TO TXT FILE') #this will be added to the quit function section of the main 
+
+check_date()
